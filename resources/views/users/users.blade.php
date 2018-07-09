5959d6adaf0f33e1b619b2527e1ab09cafea73c3
sumika.ishikawa <sumika.ishikawa@rakuten.com>
2018-07-09 03:44:47 +0000

    UI change

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -2,15 +2,19 @@
 <ul class="media-list">
 @foreach ($users as $user)
     <li class="media">
-        <div class="media-left">
-            <img class="media-object img-rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
-        </div>
-        <div class="media-body">
-            <div>
-                {{ $user->name }}
-            </div>
-            <div>
-                <p>{!! link_to_route('users.show', 'View profile', ['id' => $user->id]) !!}</p>
+    <aside class="col-xs-4">
+    </aside>
+        <div class="col-xs-8">
+            <div class="media-left">
+                <img class="media-object img-rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
+                </div>
+                    <div class="media-body">
+                        <div>
+                            {{ $user->name }}
+                        </div>
+                    <div>
+                    <p>{!! link_to_route('users.show', 'View profile', ['id' => $user->id]) !!}</p>
+                </div>
             </div>
         </div>
     </li>
