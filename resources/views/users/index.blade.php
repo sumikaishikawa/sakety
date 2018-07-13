775a803748a91af8f11efcbaa894cf47cf18077e
taito.kobayashi <taito.a.kobayashi@rakuten.com>
2018-07-13 05:56:10 +0000

    user_index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('users.users', ['users' => $users])
+@if (count($users) > 0)
+<ul class="media-list">
+    <?php $i=0; ?>
+@foreach ($users as $user)
+    <li class="media">
+    <aside class="col-xs-4">
+    </aside>
+        <div class="col-xs-8">
+            <div class="media-left">
+                <img class="media-object img-rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
+                </div>
+                    <div class="media-body">
+                        <div>
+                            {{ $user->name }}
+                        </div>
+                    <div>
+                        <p>{!! link_to_route('users.show', 'View profile', ['id' => $user->id]) !!}</p>
+                </div>
+            </div>
+        </div>
+    </li>
+    @endforeach
+    {!! $users->render() !!}
+@endif
 @endsection
