0079eef7b8c2b027864dc2b3a817bbe29a7102b4
sumika.ishikawa <sumika.ishikawa@rakuten.com>
2018-07-19 08:07:19 +0000

    no change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,14 +8,21 @@
     
     <div class='post'>
      @if (Auth::user()->id == $user->id)
-                  {!! Form::open(['route' => 'microposts.store']) !!}
-                      <div class="form-group">
-                          {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2', 'placeholder' => '目標、期日、場所、コメント']) !!}
-                          {!! Form::submit('Post', ['class' => 'btn btn-info btn-block']) !!}
-                      </div>
-                  {!! Form::close() !!}
-            @endif
-          </div>  
+          {!! Form::open(['route' => 'microposts.store']) !!}
+              <div class="form-group">
+                  <p>Target</p>
+                  {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
+                  <p>Deadline</p>
+                  <div class="input-group input-daterange">
+                    <input type="text" id="datepickerFrom" class="form-control" name="dateto_id" placeholder="開始日を選んでください" autocomplete="off">
+                    <div class="input-group-addon">to</div>
+                    <input type="text" id="datepickerTo" class="form-control" name="datefrom_id" placeholder="終了日を選んでください" autocomplete="off">
+                  </div>
+                  {!! Form::submit('Post', ['class' => 'btn btn-info btn-block']) !!}
+              </div>
+          {!! Form::close() !!}
+    @endif
+  </div>  
             
         <div class="row">
             <aside class="col-xs-2">
