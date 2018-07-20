d0f21c78d8e39b37fea3a071f58097b27aaf9ca6
sumika.ishikawa <sumika.ishikawa@rakuten.com>
2018-07-20 03:42:09 +0000

    heroku donebuttun

--- resources/views/user_done/done_button.blade.php
+++ resources/views/user_done/done_button.blade.php
@@ -5,23 +5,24 @@
         // exit;
         
         ?>
+       
+        
         <?php
         
         if(is_null($user_done)){
         ?>
-        @if (count($user_done) == 0 && Auth::id() == $user->id)
+        @if (Auth::id() == $user->id)
                 {!! Form::open(['route' => ['users.done', $user->id]]) !!}
                     {!! Form::hidden('invisible', $microposts->id) !!}
                     {!! Form::submit('doneにする', ['class' => "btn btn-default btn-xs"]) !!}
                 {!! Form::close() !!}
+        @endif
         <?php
-        }
+        
+        }else{
+        
         ?>
-        
-        
-        
-        
-        
+                
         @if (count($user_done) > 0 && Auth::id() == $user->id)
                 {!! Form::open(['route' => ['users.undone', $user->id], 'method' => 'delete']) !!}
                     {!! Form::hidden('invisible', $microposts->id) !!}
@@ -37,3 +38,27 @@
         @elseif(count($user_done) == 0 && Auth::id() != $user->id)
                 <a class="btn btn-default btn-xs" href="#" role="button">doneにする</a>
         @endif
+        
+        <?php      
+        }
+        ?>
+        
+        
+        
+        
+        
+        <!--@if (count($user_done) > 0 && Auth::id() == $user->id)-->
+        <!--        {!! Form::open(['route' => ['users.undone', $user->id], 'method' => 'delete']) !!}-->
+        <!--            {!! Form::hidden('invisible', $microposts->id) !!}-->
+        <!--            {!! Form::submit('done', ['class' => "btn btn-success btn-xs"]) !!}-->
+        <!--        {!! Form::close() !!}-->
+        <!--@elseif (count($user_done) == 0 && Auth::id() == $user->id)-->
+        <!--        {!! Form::open(['route' => ['users.done', $user->id]]) !!}-->
+        <!--            {!! Form::hidden('invisible', $microposts->id) !!}-->
+        <!--            {!! Form::submit('doneにする', ['class' => "btn btn-default btn-xs"]) !!}-->
+        <!--        {!! Form::close() !!}-->
+        <!--@elseif(count($user_done) > 0 && Auth::id() != $user->id)-->
+        <!--        <a class="btn btn-success btn-xs" href="#" role="button">done</a>-->
+        <!--@elseif(count($user_done) == 0 && Auth::id() != $user->id)-->
+        <!--        <a class="btn btn-default btn-xs" href="#" role="button">doneにする</a>-->
+        <!--@endif-->
