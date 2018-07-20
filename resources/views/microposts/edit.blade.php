ded0fde7a1339a0d37c522823861d07b1e9d73f2
emi.sasaki <emi.sasaki@rakuten.com>
2018-07-20 01:13:59 +0000

    nav

--- resources/views/microposts/edit.blade.php
+++ resources/views/microposts/edit.blade.php
@@ -30,6 +30,7 @@
                         </div>
                         <div>
                             <p>{!! nl2br(e($comment->content)) !!}</p>
+                            <input type="text" id="datepickerFrom" class="form-control" name="dateto_id" placeholder="開始日を選んでください" autocomplete="off">
                         </div>
                         <div class="button-inline button-group">
                         <!--@if (Auth::id() == $comment->user_id)-->
